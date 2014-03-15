9f94df9b2f1e16b32727cecd932a26b5ee0266b1
John Hinrichs <jmhinric@gmail.com>
2014-03-15 17:59:30 -0400

    Add id and persistence to checkboxes

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -2,16 +2,22 @@ var todoFieldInput = $("input");
 
 $("form").submit(function(e) {
 	e.preventDefault();
-	$.post("/newtodo", { todo: todoFieldInput.val() })
-	$("<li>").text(todoFieldInput.val()).append('<input type="checkbox">').appendTo("ul");
+	$.post("/newtodo", { todo: todoFieldInput.val() }, function(response) {
+    addTodoLi(response);
+  });
 	this.reset();
 });
 
 $.getJSON("/new", function(response) {
-	$.each(response, function(index, text) {
-		$("<li>").text(text.todo).appendTo("ul"); 
+	$.each(response, function(index, todoItem) {
+		addTodoLi(todoItem);
 	});
 });
+
+function addTodoLi(todoItem) {
+  var liItem = $("<li>").text(todoItem.todo).append("<input type='checkbox'>");
+  liItem.attr("id", "todo-" + todoItem.id).appendTo("ul");
+}
 
 // $('input:checkbox').live('change', function(){
 //     if($(this).is(':checked')){
